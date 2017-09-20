366e95491106e91373c02c2cc45a12f905c58225
Ellen Cooper <ellencooper74@gmail.com>
2017-09-20 15:34:29 -0600

    adds jquery capture of form values

--- app/assets/javascripts/suggestions.js
+++ app/assets/javascripts/suggestions.js
@@ -1,30 +1,30 @@
 $(document).ready(function() {
-  fetchComments()
-  createComment()
+  getSuggestion()
 })
 
 
 function getSuggestion() {
-  $("#create-comment").on("click", function() {
-    var commentParams = {
-      comment: {
-        message: $("#comment-message").val()
-      }
-
+  $("#btn-suggest").on("click", function() {
+    var suggestionParams = {
+        occasion: $("#occassion").val(),
+        season: $("#season").val(),
+        food: $("#food").val(),
+        price: $("#price").val(),
     }
+    console.log(suggestionParams)
 
-    $.ajax({
-      type:    "POST",
-      url:     "/api/v1/leagues/" + $("#league-id").val() + "/comments",
-      data:    commentParams,
-      success: function(newComment) {
-        $('#league-comments').html('');
-        document.getElementById("comment-message").value = "";
-        fetchComments()
-      },
-      error: function(xhr) {
-        console.log(xhr.responseText)
-      }
-    })
+    // $.ajax({
+    //   type:    "POST",
+    //   url:     "/api/v1/leagues/" + $("#league-id").val() + "/comments",
+    //   data:    commentParams,
+    //   success: function(newComment) {
+    //     $('#league-comments').html('');
+    //     document.getElementById("comment-message").value = "";
+    //     fetchComments()
+    //   },
+    //   error: function(xhr) {
+    //     console.log(xhr.responseText)
+    //   }
+    // })
   })
 }
